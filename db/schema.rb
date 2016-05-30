dfd7a5c93cddd67f3c85cd9a21ef3b5cd8bd323e
Eric Dallo <ericdallo06@hotmail.com>
2016-05-30 00:36:27 -0300

    Update files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714015240) do
+ActiveRecord::Schema.define(version: 20150804234330) do
 
   create_table "clientes", force: :cascade do |t|
     t.string   "name",             limit: 255
